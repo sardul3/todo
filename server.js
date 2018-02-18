ba44cf019d319d9156b43928b56f8fc84f1e2f99
Sagar Poudel <poudels@warhawks.ulm.edu>
2018-02-18 13:21:57 -0600

    updated the patch route

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const _ = require('lodash');
 
 const {User} = require('./server/models/user');
 const {Todo} = require('./server/models/todo');
@@ -60,6 +61,31 @@ app.delete("/todos/:id", (req, res)=>{
   },()=>{
     res.status(400).send();
   });
+});
+
+app.patch('/todos/:id', (req, res)=>{
+  var id = req.params.id;
+  var body = _.pick(req.body, ["text", "completed"]);
+    if(!ObjectID.isValid(id)){
+      return res.status(404).send();
+    }
+    if(_.isBoolean(body.completed) && body.completed){
+      body.completedAt = new Date().getTime();
+    }
+    else{
+      body.completed = false;
+      body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo)=>{
+      if(!todo){
+        res.status(404).send();
+      }
+        res.send({todo});
+    }).catch((e)=>{
+      res.status(400).send();
+    });
+
 });
 
 
