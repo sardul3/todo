50457a20f738516b6fae128f642a98d979bfb70a
Sagar Poudel <poudels@warhawks.ulm.edu>
2018-02-17 22:41:35 -0600

    d

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const {User} = require('./server/models/user');
-const {Todo} = require('./server/models/todo');
-const {mongoose} = require('./server/db/mongoose');
+const {User} = require('../server/models/user');
+const {Todo} = require('../server/models/todo');
+const {mongoose} = require('../server/db/mongoose');
 
 const {ObjectID} = require('mongodb');
 
