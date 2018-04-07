24146ee25798ed406615f0c64b990ba231499a17
Sagar Poudel <poudels@warhawks.ulm.edu>
2018-04-06 19:57:32 -0500

    finished version

--- server.js
+++ server.js
@@ -13,8 +13,11 @@ const {authenticate} = require('./server/middleware/authenticate');
 var app = express();
 app.use(bodyParser.json());
 
-app.post("/todos", (req, res)=>{
-  var todo = new Todo({text: req.body.text});
+app.post("/todos", authenticate, (req, res)=>{
+  var todo = new Todo({
+    text: req.body.text,
+    _creator: req.user._id
+  });
   todo.save().then((result)=>{
     res.send(result);
   },
@@ -23,20 +26,25 @@ app.post("/todos", (req, res)=>{
 });
 });
 
-app.get("/todos", (req, res)=>{
-  Todo.find().then((result)=>{
+app.get("/todos", authenticate, (req, res)=>{
+  Todo.find(
+    {_creator: req.user._id}).then((result)=>{
     res.send({result});
   },(error)=>{
     console.log(error);
   });
 });
 
-app.get('/todos/:id', (req,res)=>{
+app.get('/todos/:id', authenticate, (req,res)=>{
   var id = req.params.id;
   if(! ObjectID.isValid(id)){
     return res.status(404).send();
   }
-  Todo.findById(id).then((todo)=>{
+
+  Todo.findOne({
+      _id: id,
+    _creator: req.user._id
+  }).then((todo)=>{
     if(! todo){
       return res.status(404).send();
     }
@@ -48,12 +56,15 @@ app.get('/todos/:id', (req,res)=>{
 
 });
 
-app.delete("/todos/:id", (req, res)=>{
+app.delete("/todos/:id", authenticate, (req, res)=>{
   var id = req.params.id;
   if(!ObjectID.isValid(id)){
     return res.status(404).send();
   }
-  Todo.findByIdAndRemove(id).then((todo)=>{
+  Todo.findOneAndRemove({
+    _id:id,
+    _creator: req.user._id
+   }).then((todo)=>{
     if(!todo){
       res.status(404).send();
     }
@@ -64,7 +75,7 @@ app.delete("/todos/:id", (req, res)=>{
   });
 });
 
-app.patch('/todos/:id', (req, res)=>{
+app.patch('/todos/:id', authenticate, (req, res)=>{
   var id = req.params.id;
   var body = _.pick(req.body, ["text", "completed"]);
     if(!ObjectID.isValid(id)){
@@ -78,7 +89,7 @@ app.patch('/todos/:id', (req, res)=>{
       body.completedBy = null;
     }
 
-    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo)=>{
+    Todo.findOneAndUpdate({_id:id, _creator: req.user._id}, {$set: body}, {new: true}).then((todo)=>{
       if(!todo){
         res.status(404).send();
       }
@@ -104,6 +115,26 @@ app.post('/users', (req,res)=>{
       res.header('x-auth', token).send(user);
   }).catch((e)=>{
     res.status(400).send(e);
+  });
+});
+
+app.post('/users/login', (req,res)=>{
+  var body = _.pick(req.body, ["email", "password"]);
+
+  User.findByCredentials(body.email, body.password).then((user)=>{
+    return user.generateAuthToken().then((token)=>{
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e)=>{
+      res.status(400).send(e);
+  });
+});
+
+app.delete("/users/me/token", authenticate, (req,res)=>{
+   req.user.deleteToken(req.token).then(()=>{
+    res.status(200).send();
+  }, ()=>{
+    res.status(400).send();
   });
 });
 
--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 
 var UserSchema = new mongoose.Schema({
@@ -51,6 +52,27 @@ return  User.findOne({
   });
 };
 
+
+UserSchema.statics.findByCredentials = function(email, password){
+  var User = this;
+
+return  User.findOne({email}).then((user)=>{
+    if(!user){
+      return Promise.reject();
+    }
+  return  new Promise((resolve, reject)=>{
+    bcrypt.compare(password, user.password, (err, result)=>{
+      if(result){
+        resolve(user);
+      }
+      else{
+        reject();
+      }
+    });
+  });
+    });
+};
+
 UserSchema.methods.toJSON = function(){
   var user = this;
   var userObject = user.toObject();
@@ -69,6 +91,33 @@ UserSchema.methods.generateAuthToken = function(){
     return token;
   });
 };
+
+UserSchema.methods.deleteToken = function(token){
+  var user = this;
+
+return  user.update({
+    $pull:{
+      tokens:{token}
+    }
+  });
+};
+
+UserSchema.pre('save', function(next){
+  var user = this;
+  if(user.isModified('password')){
+      bcrypt.genSalt(10, (err, salt)=>{
+        bcrypt.hash(user.password, salt, (err, hash)=>{
+          user.password = hash;
+            next();
+        });
+      });
+
+  }
+  else{
+    next();
+  }
+});
+
 
 var User = mongoose.model('User', UserSchema);
 
